c7ec4853bb0528e70b212b6633d35f81b07613c3
zhixiang <zhixiang96@hotmail.com>
2017-07-11 21:41:40 +0800

    update

--- js/bella.js
+++ js/bella.js
@@ -1,15 +1,15 @@
 /*
-*
-*
-*   bella.js version:       0.1
-*   element list version:   0.1
-*
-*
-* */
-
+ *
+ *
+ *   bella.js version:       0.1
+ *   element list version:   0.1
+ *
+ *
+ * */
 //element list
 var globalElements = [
 
+    //elements need to be initialized by JavaScript
     //layout::
     '.bl-tab-view',
 
@@ -29,53 +29,100 @@ $(document).ready(function () {
     blClassGlbCov();
 
     //call highlight.js
-    hljs();
-
+    // hljs();
     //
-
 });
+bella = {
+    version: "0.1",
 
+    //timestamp
+
+    getTimeStamp: function () {
+        return new Date().getTime();
+    },
+    scrollToTop: function () {
+        window.scrollBy(0, -10);
+        scrolldelay = setTimeout('pageScroll()', 100);
+        if (document.documentElement.scrollTop == 0) clearTimeout(scrolldelay);
+    },
+    //notification
+    notify: function (msg) {
+        blNotificationCov("bl-notification-normal", msg);
+        // NotificationTimer = setTimeout("bella.hideNotification()",5000);
+    },
+    alert: function (msg) {
+        blNotificationCov("bl-notification-alert", msg);
+        // NotificationTimer = setTimeout("bella.hideNotification()",5000);
+
+    },
+    hideNotification: function () {
+        $('.bl-notification-alert button').remove();
+        $('.bl-notification-normal button').remove();
+
+        $('.bl-notification-normal').slideUp(500, function () {
+            $('.bl-notification-normal').remove();
+        });
+        $('.bl-notification-alert').slideUp(500, function () {
+            $('.bl-notification-alert').remove();
+        });
+    },
+
+    //view operations
+    switchTabFocus: function (father, NO) {
+        $(father).children('*').removeClass('.bl-tab-view-active');
+        $(father).find('.bl-view-header li').get(NO).addClass('.bl-tab-view-active');
+    }
+};
 
 function hljs() {
-    $('pre code').each(function(i, block) {
+    $('pre code').each(function (i, block) {
         hljs.highlightBlock(block);
     });
 }
 //
 //
+
 function blClassGlbCov() {
 
     //bellaConvertIterator
     globalElements.forEach(function (e) {
         var $eleList = $(e).toArray();
         $eleList.forEach(function (eleListMember) {
-            var funcName = String(e.replace(/-/,''));
-            while (funcName.indexOf('-')>0)
-                funcName = funcName.replace(/-/,'');
-            funcName = funcName.replace(/./,'') + 'Cov(eleListMember)';
+            var funcName = String(e.replace(/-/, ''));
+            while (funcName.indexOf('-') > 0)
+                funcName = funcName.replace(/-/, '');
+            funcName = funcName.replace(/./, '') + 'Cov(eleListMember)';
             eval(funcName);
         });
     });
 }
-
+function blNotificationCov(type, msg) {
+    // clearTimeout(NotificationTimer);
+    var currTimeStamp = bella.getTimeStamp();
+    var notifDiv = $('<div style="display: none" id="' + currTimeStamp + '" class="' + type + ' raw-w"><div class="raw-f"><p></p></div><div onclick="bella.hideNotification()" class="tiny bl-notification-hide-button">Hide</div></div>');
+    $('body').before(notifDiv);
+    $('#' + currTimeStamp + ' p').html(msg);
+    $('.' + type + '').slideUp(500);
+    $('#' + currTimeStamp).slideDown(500);
+}
 function bltabviewCov(oneTabView) {
     var $oneTabView = $(oneTabView);
     var oneTabViewList = $oneTabView.find('.bl-view-header li').toArray();
     var oneTabViewBodyList = $oneTabView.find('.bl-view-body').toArray();
     var fullWid = parseFloat($oneTabView.find('.bl-view-header').css('width'));
-    var memberWid = fullWid/oneTabViewList.length;
+    var memberWid = fullWid / oneTabViewList.length;
     oneTabViewList.forEach(function (oneTabViewListMember) {
         $(oneTabViewListMember).css({
-            'width':memberWid
+            'width': memberWid
         });
     });
     $(oneTabViewList[0]).addClass('bl-tab-view-active');
-    $(oneTabViewBodyList[0]).css({'display':'block'});
+    $(oneTabViewBodyList[0]).css({'display': 'block'});
     oneTabViewList.forEach(function (oneTabViewListMember) {
         var $oneTabViewListMember = $(oneTabViewListMember);
-        $oneTabViewListMember.on('click',function () {
-            $(oneTabViewBodyList).css({'display':'none'});
-            $(oneTabViewBodyList[oneTabViewList.indexOf($oneTabViewListMember.get(0))]).css({'display':'block'});
+        $oneTabViewListMember.on('click', function () {
+            $(oneTabViewBodyList).css({'display': 'none'});
+            $(oneTabViewBodyList[oneTabViewList.indexOf($oneTabViewListMember.get(0))]).fadeIn(100);
             $oneTabView.find('li').removeClass('bl-tab-view-active');
             $oneTabViewListMember.addClass('bl-tab-view-active');
 
@@ -87,17 +134,17 @@ function attachdelegateCov(oneAttachDelegate) {
     $(document).scroll(function () {
         var scrollTop = $(document).scrollTop();
         var objScrollTop = $oneAttachDelegate.position().top;
-        if(scrollTop > objScrollTop && $('.attach-delegate-clone').length == 0){
+        if (scrollTop > objScrollTop && $('.attach-delegate-clone').length == 0) {
             var $attachDelegateInstant = $oneAttachDelegate.clone(true);
             $oneAttachDelegate.append($attachDelegateInstant);
             $attachDelegateInstant.addClass('attach-delegate-clone');
             $attachDelegateInstant.css({
-                'position':'fixed',
-                'top':$oneAttachDelegate.position().top - parseFloat($oneAttachDelegate.css('height')) + 'px',
-                'left':$oneAttachDelegate.position().left + 'px',
-                'z-index':500
+                'position': 'fixed',
+                'top': $oneAttachDelegate.position().top - parseFloat($oneAttachDelegate.css('height')) + 'px',
+                'left': $oneAttachDelegate.position().left + 'px',
+                'z-index': 500
             });
-        }else if(scrollTop <= objScrollTop){
+        } else if (scrollTop <= objScrollTop) {
             $(document).find($('.attach-delegate-clone')).remove();
         }
     });
@@ -105,26 +152,26 @@ function attachdelegateCov(oneAttachDelegate) {
 function blpageCov(onePage) {
     var $pageRoot = $(onePage);
     var converter = new showdown.Converter({
-        "omitExtraWLInCodeBlocks":true,
-        "noHeaderId":false,
-        "prefixHeaderId":"",
-        "ghCompatibleHeaderId":true,
-        "headerLevelStart":1,
-        "parseImgDimensions":true,
-        "simplifiedAutoLink":true,
-        "excludeTrailingPunctuationFromURLs":false,
-        "literalMidWordUnderscores":true,
-        "strikethrough":true,
-        "tables":true,
-        "tablesHeaderId":false,
-        "ghCodeBlocks":true,
-        "tasklists":true,
-        "smoothLivePreview":true,
-        "smartIndentationFix":false,
-        "disableForced4SpacesIndentedSublists":false,
-        "simpleLineBreaks":false,
-        "requireSpaceBeforeHeadingText":false,
-        "ghMentions":false,"extensions":[],"sanitize":false
+        "omitExtraWLInCodeBlocks": true,
+        "noHeaderId": false,
+        "prefixHeaderId": "",
+        "ghCompatibleHeaderId": true,
+        "headerLevelStart": 1,
+        "parseImgDimensions": true,
+        "simplifiedAutoLink": true,
+        "excludeTrailingPunctuationFromURLs": false,
+        "literalMidWordUnderscores": true,
+        "strikethrough": true,
+        "tables": true,
+        "tablesHeaderId": false,
+        "ghCodeBlocks": true,
+        "tasklists": true,
+        "smoothLivePreview": true,
+        "smartIndentationFix": false,
+        "disableForced4SpacesIndentedSublists": false,
+        "simpleLineBreaks": false,
+        "requireSpaceBeforeHeadingText": false,
+        "ghMentions": false, "extensions": [], "sanitize": false
     });
 
     var $pageParagList = $pageRoot.find('p').toArray();
@@ -139,7 +186,7 @@ function blsidebarCov(oneSidebar) {
     $oneSidebar.children('a:first').addClass('bl-sidebar-active');
     $oneSidebarMember.forEach(function (oneSidebarbarMemberA) {
         var $oneSidebarbarMemberA = $(oneSidebarbarMemberA);
-        $oneSidebarbarMemberA.on('click',function () {
+        $oneSidebarbarMemberA.on('click', function () {
             $oneSidebar.children('a').removeClass('bl-sidebar-active');
             $oneSidebarbarMemberA.addClass('bl-sidebar-active');
         });
@@ -162,26 +209,27 @@ function blnavCov(oneBlHeaderListMember) {
         $itemListMemberUl.before("<i class=\"fa fa-angle-down\"></i>");
 
         //
-        $itemListMember.on('click',function () {
-            if($itemListMemberUl.hasClass('bl-nav-active'))
-                $itemListMemberUl.fadeOut(100,function () {
-                    $('.bl-nav-active').css('display','none').removeClass('bl-nav-active');
+        $itemListMember.on('click', function () {
+            if ($itemListMemberUl.hasClass('bl-nav-active'))
+                $itemListMemberUl.fadeOut(100, function () {
+                    $('.bl-nav-active').css('display', 'none').removeClass('bl-nav-active');
                 });
             else {
                 $('.bl-nav-active')
-                    .css('display','none')
+                    .css('display', 'none')
                     .removeClass('bl-nav-active');
                 $itemListMemberUl
                     .addClass('bl-nav-active')
                     .css({
-                    'top':$itemListMember.position().top + 'px' + $itemListMember.css('height'),
-                    'left':$itemListMember.position().left + 'px'});
+                        'top': $itemListMember.position().top + 'px' + $itemListMember.css('height'),
+                        'left': $itemListMember.position().left + 'px'
+                    });
                 $itemListMemberUl.fadeIn(100);
             }
         });
         $itemListMemberUl.mouseleave(function () {
-            $itemListMemberUl.fadeOut(100,function () {
-                $('.bl-nav-active').css('display','none').removeClass('bl-nav-active');
+            $itemListMemberUl.fadeOut(100, function () {
+                $('.bl-nav-active').css('display', 'none').removeClass('bl-nav-active');
             });
         })
     });
