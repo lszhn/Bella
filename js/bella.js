3fe1a3cb87699d17970d37fefc1fd7f235e06681
zhixiang <zhixiang96@hotmail.com>
2017-08-27 20:14:17 +0800

    update

--- js/bella.js
+++ js/bella.js
@@ -17,17 +17,23 @@ var globalElements = [
     '.bl-nav',
     '.bl-sidebar',
     '.bl-page',
+    '.bl-view',
 
     //delegate
     '.attach-delegate'
 
+
 ];
+var globalStaticClasses = [
+
+    '.switch-delegate'
+];
+
 
 $(document).ready(function () {
 
     //initialize global bella class
     blClassGlbCov();
-
     //call highlight.js
     // hljs();
     //
@@ -35,6 +41,26 @@ $(document).ready(function () {
 bella = {
     version: "0.1",
 
+    //MobileSupport
+    mobileSupport: function () {
+        $('title').after('<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, target-densitydpi=device-dpi" />');
+        document.documentElement.addEventListener('touchstart', function (event) {
+            if (event.touches.length > 1) {
+                event.preventDefault();
+            }
+        }, false);
+        var lastTouchEnd = 0;
+        document.documentElement.addEventListener('touchend', function (event) {
+            var now = Date.now();
+            if (now - lastTouchEnd <= 300) {
+                event.preventDefault();
+            }
+            lastTouchEnd = now;
+        }, false);
+    },
+    getWindowHeight: function () {
+        return $(window).height();
+    },
     //timestamp
 
     getTimeStamp: function () {
@@ -65,12 +91,6 @@ bella = {
         $('.bl-notification-alert').slideUp(500, function () {
             $('.bl-notification-alert').remove();
         });
-    },
-
-    //view operations
-    switchTabFocus: function (father, NO) {
-        $(father).children('*').removeClass('.bl-tab-view-active');
-        $(father).find('.bl-view-header li').get(NO).addClass('.bl-tab-view-active');
     }
 };
 
@@ -79,6 +99,7 @@ function hljs() {
         hljs.highlightBlock(block);
     });
 }
+
 //
 //
 
@@ -95,16 +116,44 @@ function blClassGlbCov() {
             eval(funcName);
         });
     });
+    blStaticClassGlbCov();
 }
+
+function blStaticClassGlbCov() {
+
+    //bellaConvertIterator
+    globalStaticClasses.forEach(function (e) {
+        var funcName = String(e.replace(/-/, ''));
+        while (funcName.indexOf('-') > 0)
+            funcName = funcName.replace(/-/, '');
+        funcName = funcName.replace(/./, '') + 'Cov()';
+        eval(funcName);
+    });
+}
+
+function blviewCov(blviewTotalSet) {
+
+    var $blviewTotalSet = $(blviewTotalSet);
+    $blviewTotalSet.toArray().forEach(function (oneblview) {
+        var $oneblviewbodyTotalSet = $(oneblview).find('.bl-view-body');
+        var oneblviewbodyTotalList = $oneblviewbodyTotalSet.toArray();
+        oneblviewbodyTotalList.forEach(function (oneblviewbody) {
+            $(oneblviewbody).css("display", "none");
+        });
+        $(oneblviewbodyTotalList[0]).css("display", "block");
+    });
+}
+
 function blNotificationCov(type, msg) {
     // clearTimeout(NotificationTimer);
     var currTimeStamp = bella.getTimeStamp();
     var notifDiv = $('<div style="display: none" id="' + currTimeStamp + '" class="' + type + ' raw-w"><div class="raw-f"><p></p></div><div onclick="bella.hideNotification()" class="tiny bl-notification-hide-button">Hide</div></div>');
     $('body').before(notifDiv);
     $('#' + currTimeStamp + ' p').html(msg);
-    $('.' + type + '').slideUp(500);
-    $('#' + currTimeStamp).slideDown(500);
+    $('.' + type + '').slideUp(300);
+    $('#' + currTimeStamp).slideDown(300);
 }
+
 function bltabviewCov(oneTabView) {
     var $oneTabView = $(oneTabView);
     var oneTabViewList = $oneTabView.find('.bl-view-header li').toArray();
@@ -122,13 +171,33 @@ function bltabviewCov(oneTabView) {
         var $oneTabViewListMember = $(oneTabViewListMember);
         $oneTabViewListMember.on('click', function () {
             $(oneTabViewBodyList).css({'display': 'none'});
-            $(oneTabViewBodyList[oneTabViewList.indexOf($oneTabViewListMember.get(0))]).fadeIn(100);
+            $(oneTabViewBodyList[oneTabViewList.indexOf($oneTabViewListMember.get(0))]).css({'display': 'block'});
             $oneTabView.find('li').removeClass('bl-tab-view-active');
             $oneTabViewListMember.addClass('bl-tab-view-active');
 
         });
     });
 }
+
+function switchdelegateCov() {
+    var $switchDelegateTotalList = $('.switch-delegate').toArray();
+    $switchDelegateTotalList.forEach(function (oneswitchDelegate) {
+        var oneSlotName = $(oneswitchDelegate).attr("name");
+        var slotList = $("ul[name=" + oneSlotName + "]:not(\".switch-delegate\")").find('.bl-view-body');
+        var oneSwitchDelegateAList = $(oneswitchDelegate).find('a').toArray();
+        oneSwitchDelegateAList.forEach(function (oneSwitchDelegateAListMember) {
+            $(oneSwitchDelegateAListMember).on('click', function () {
+                $(oneSwitchDelegateAList).removeClass("bl-view-active");
+                $(oneSwitchDelegateAListMember).addClass("bl-view-active");
+                $(slotList).css('display', 'none');
+                $(slotList[oneSwitchDelegateAList.indexOf(oneSwitchDelegateAListMember)]).css('display', 'block');
+                $('.m-bl-view-header').text($(oneSwitchDelegateAListMember).text());
+            });
+        });
+        $(oneSwitchDelegateAList[0]).addClass("bl-view-active");
+    });
+}
+
 function attachdelegateCov(oneAttachDelegate) {
     var $oneAttachDelegate = $(oneAttachDelegate);
     $(document).scroll(function () {
@@ -149,6 +218,7 @@ function attachdelegateCov(oneAttachDelegate) {
         }
     });
 }
+
 function blpageCov(onePage) {
     var $pageRoot = $(onePage);
     var converter = new showdown.Converter({
@@ -180,6 +250,7 @@ function blpageCov(onePage) {
         $oneParag.html(converter.makeHtml($oneParag.html()));
     });
 }
+
 function blsidebarCov(oneSidebar) {
     var $oneSidebar = $(oneSidebar);
     var $oneSidebarMember = $oneSidebar.children('a').toArray();
@@ -206,14 +277,16 @@ function blnavCov(oneBlHeaderListMember) {
     $itemList.forEach(function (itemListMember) {
         var $itemListMember = $(itemListMember);
         var $itemListMemberUl = $itemListMember.children('ul');
-        $itemListMemberUl.before("<i class=\"fa fa-angle-down\"></i>");
+        // $itemListMemberUl.before("<i class=\"fa fa-angle-down\"></i>");
 
         //
         $itemListMember.on('click', function () {
-            if ($itemListMemberUl.hasClass('bl-nav-active'))
-                $itemListMemberUl.fadeOut(100, function () {
-                    $('.bl-nav-active').css('display', 'none').removeClass('bl-nav-active');
-                });
+            if ($itemListMemberUl.hasClass('bl-nav-active')) {
+                // $itemListMemberUl.fadeOut(100, function () {
+                //
+                //     $('.bl-nav-active').css('display', 'none').removeClass('bl-nav-active');
+                // })
+            }
             else {
                 $('.bl-nav-active')
                     .css('display', 'none')
--- readme.md
+++ readme.md
@@ -1,19 +1,24 @@
-#   developers' tutorial
-_NOTICE:_
->if you are actually developing a project,trust me, try BootStrap instead of this bullshit.
-##  layout
-###   baseline
-A number called "baseline" must 
-be set up correctly in 
-every Bella page.(default 40px)  
+#   bellaFront 开发者指南
+_建议_
+>bellaFront（以下简称"bella"）仍然是开发中的框架，API变更与框架源关闭是随时可能发生的。
+##  框架结构
+###   栅格布局
+bella 为开发者提供全局的布局(default 40px)，开发者通过定义
+行宽与列高来
 what does _baseline_ mean?  
 The body of a bella page is divided into 24 parts
 , so you can guess that the _baseline_ is the width 
 of each part, so try set up a suitable number.
 ### raw
-*raw-{WIDTH}  
+*   raw-{WIDTH}  
 create a raw with width multiplied baseline by _WIDTH_
-*raw-f
+*   raw-f
 create a full-body width(24 * baseline) raw.  
-*raw-w  
+*   raw-w  
 create a full-screen width(100%) raw.
+
+##  Elements
+
+bella elements 
+
+_bl_
